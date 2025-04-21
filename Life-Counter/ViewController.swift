e4df03ac1d334a870ad6b6b3e1f5b3a9d934fca3
TristanK117 <81729665+TristanK117@users.noreply.github.com>
2025-04-21 09:20:25 -0700

    first

---
+++ Life-Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Life-Counter
+//
+//  Created by Tristan Khieu on 4/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Life-CounterTests/Life_CounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Life_CounterTests.swift
+//  Life-CounterTests
+//
+//  Created by Tristan Khieu on 4/21/25.
+//
+
+import Testing
+@testable import Life_Counter
+
+struct Life_CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
